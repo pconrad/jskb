c9cfc4e41c93416411714cc613f4b39cf58fd216
Phill Conrad <pconrad@cs.ucsb.edu>
2016-04-29 17:48:10 -0700

    initial white key demo

---
+++ jskb.js
@@ -0,0 +1,12 @@
+
+
+function keyDimensions(whiteKeyWidth) {
+	return {
+ 		"whiteKeyWidth" : whiteKeyWidth,
+		"whiteKeyHeight" : whiteKeyWidth * (6.0 / (7.0/8.0)),
+		"blackKeyWidth" : whiteKeyWidth * (3.0 / 7.0),
+		"blackKeyHeight" : whiteKeyWidth * (3.5 / (7.0 / 8.0) )
+	};
+}
+
+
